d6d67e85b304a3470504d2cdd7a6d04dac12f15e
Nycolas Santana <nycolassantana00@gmail.com>
2021-10-08 19:12:48 -0300

    melhorado o codigo do card dos produtos iniciado o carrousel de propagandas

--- src/components/Cards/Card.jsx
+++ src/components/Cards/Card.jsx
@@ -6,21 +6,31 @@ import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
 const CardCategory = ({
   title,
+  subTitle = '',
+  value,
   className = '',
   imageSrc = '',
   altText = '',
 }) => {
 
-  const productValue = 850
+  const productValue12x = (Math.floor(value / 12).toLocaleString('pt-br',{style: 'currency', currency: 'BRL'}))
 
-  const productValue12x = (Math.floor(productValue / 12))
+  function converterValorParaReal(numero) {
+    if(typeof numero == 'number') {
+      let numeroConvertido = numero.toLocaleString('pt-br',{style: 'currency', currency: 'BRL'})
+      return numeroConvertido
+    } else {
+      return numero
+    }
+  }
 
   const isProduct = (
     <div className="ProductCardContainer">
       <hr className="ProductCardLine" />
-      <h3 className="ProductCardTitle">{title}</h3>
-      <p className="ProductCardValue">R$ {productValue}.00</p>
-      <p className="ProductCardValueInstallment">12x R$ {productValue12x}.00 sem juros</p>
+      <p className="ProductCardTitle">{title}</p>
+      <p className="ProductCardSubTItle">{subTitle}</p>
+      <p className="ProductCardValue">{converterValorParaReal(value)}</p>
+      <p className="ProductCardValueInstallment">12x R$ {productValue12x}</p>
       <div className="ProductCardButtons">
         <button className="ProductCardBuyButton">Comprar</button>
         <button className="ProductCardCartButton"><FontAwesomeIcon icon={faShoppingCart} /></button>
--- src/components/Cards/ProductCard.jsx
+++ src/components/Cards/ProductCard.jsx
@@ -8,6 +8,16 @@ import TecladoGamerMecanicoKumaraK552 from './ProductsImgs/TecladoGamerMecanicoK
 import TecladoGamerRedDragonKarura from './ProductsImgs/TecladoGamerRedDragonKarura.png'
 import MousePadGamerInfernalDragon from './ProductsImgs/MousePadGamerInfernalDragon.png'
 import HeadSetGamerRedDraonZeusX from './ProductsImgs/HeadSetGamerRedDraonZeusX.png'
+import HeadSetGamerSemFioLogitechG733 from './ProductsImgs/HeadSetGamerSemFioLogitechG733.png'
+import MonitorGamerBlureCaseFullHd165HzBM248GC from './ProductsImgs/MonitorGamerBlueCase23.8FullHd165HzBM248GC.png'
+import MonitorLGLed238WidescreenFullHD24mk430h from './ProductsImgs/MonitorLGLed23.8WidescreenFullHD24mk430h.png'
+import MouseGamerLogitechG305LightSpeed from './ProductsImgs/MouseGamerLogitechG305LightSpeed.png'
+import MouseGamerLogitechG403Hero from './ProductsImgs/MouseGamerLogitechG403Hero.png'
+import MousePadGamerExtraGrandeLogitechG840 from './ProductsImgs/MousePadGamerExtraGrandeLogitechG840.png'
+import MousePadGamerRedDragonArchelonP002 from './ProductsImgs/MousePadGamerRedDragonArchelonP002.png'
+import MousePadLogitechPowerPlayG943 from './ProductsImgs/MousePadLogitechPowerPlayG943.png'
+import PCGamerIntelCore58GbHd500GbGeforceKit from './ProductsImgs/PCGamerIntelCore58GbHd500GbGeforceKit.png'
+import TecladoGamerLogitechG213Prodigy from './ProductsImgs/TecladoGamerLogitechG213Prodigy.png'
 
 import Card from './Card.jsx'
 
@@ -19,21 +29,37 @@ let icons = {
   "TecladoGamerRedDragonKarura": TecladoGamerRedDragonKarura,
   "MousePadGamerInfernalDragon": MousePadGamerInfernalDragon,
   "HeadSetGamerRedDraonZeusX": HeadSetGamerRedDraonZeusX,
+  "HeadSetGamerSemFioLogitechG733": HeadSetGamerSemFioLogitechG733,
+  "MonitorGamerBlureCaseFullHd165HzBM248GC": MonitorGamerBlureCaseFullHd165HzBM248GC,
+  "MonitorLGLed238WidescreenFullHD24mk430h": MonitorLGLed238WidescreenFullHD24mk430h,
+  "MouseGamerLogitechG305LightSpeed": MouseGamerLogitechG305LightSpeed,
+  "MouseGamerLogitechG403Hero": MouseGamerLogitechG403Hero,
+  "MousePadGamerExtraGrandeLogitechG840": MousePadGamerExtraGrandeLogitechG840,
+  "MousePadGamerRedDragonArchelonP002": MousePadGamerRedDragonArchelonP002,
+  "MousePadLogitechPowerPlayG943": MousePadLogitechPowerPlayG943,
+  "PCGamerIntelCore58GbHd500GbGeforceKit": PCGamerIntelCore58GbHd500GbGeforceKit,
+  "TecladoGamerLogitechG213Prodigy": TecladoGamerLogitechG213Prodigy,
 };
 
 const ProductCard = () => {
   return (
     <div className="limitPageWidth">
       <section className="flexCardsProduct">
-        <Card className="Product" title="RedDragon Cobra" imageSrc={icons["MouseGamerRedDragonCobra"]} altText="ProductIcon" />
-        <Card className="Product" title="RedDragon Griffin" imageSrc={icons["MouseGamerRedDragonGriffin"]} altText="ProductIcon" />
-        <Card className="Product" title="TecladoKumaraK552" imageSrc={icons["TecladoGamerMecanicoKumaraK552"]} altText="ProductIcon" />
-        <Card className="Product" title="TecladoRedDragonKarura" imageSrc={icons["TecladoGamerRedDragonKarura"]} altText="ProductIcon" />
-        <Card className="Product" title="RedDragon Cobra" imageSrc={icons["MouseGamerRedDragonCobra"]} altText="ProductIcon" />
-        <Card className="Product" title="MousePadInfernalDragon" imageSrc={icons["MousePadGamerInfernalDragon"]} altText="ProductIcon" />
-        <Card className="Product" title="TecladoKumaraK552" imageSrc={icons["TecladoGamerMecanicoKumaraK552"]} altText="ProductIcon" />
-        <Card className="Product" title="HeadSetRedDraonZeusX" imageSrc={icons["HeadSetGamerRedDraonZeusX"]} altText="ProductIcon" />
-        <Card className="Product" title="Razer" imageSrc={icons["MouseGamerRedDragonGriffin"]} altText="ProductIcon" />
+        <Card className="Product" title="RedDragon Cobra" subTitle="Mouse Gamer Cobra M711" imageSrc={icons["MouseGamerRedDragonCobra"]} value={135} altText="ProductIcon" />
+        <Card className="Product" title="RedDragon Griffin" subTitle="Mouse Gamer Griffin" imageSrc={icons["MouseGamerRedDragonGriffin"]} value={110} altText="ProductIcon" />
+        <Card className="Product" title="Kumara K552" subTitle="Teclado Kumara K552" imageSrc={icons["TecladoGamerMecanicoKumaraK552"]} value={230} altText="ProductIcon" />
+        <Card className="Product" title="RedDragon Karura" subTitle="Teclado RedDragon Karura" imageSrc={icons["TecladoGamerRedDragonKarura"]} value={150} altText="ProductIcon" />
+        <Card className="Product" title="MousePad Infernal Dragon" subTitle="MousePad Infernal Dragon" imageSrc={icons["MousePadGamerInfernalDragon"]} value={149.90} altText="ProductIcon" />
+        <Card className="Product" title="RedDraon ZeusX" subTitle="HeadSet RedDraon ZeusX" imageSrc={icons["HeadSetGamerRedDraonZeusX"]} value={369} altText="ProductIcon" />
+        <Card className="Product" title="MonitorGamerBlur" subTitle="Monitor Gamer Blur" imageSrc={icons["MonitorGamerBlureCaseFullHd165HzBM248GC"]} value={1899.90} altText="ProductIcon" />
+        <Card className="Product" title="Monitor LGLed 24MK430H" subTitle="Monitor LGLed FullHD" imageSrc={icons["MonitorLGLed238WidescreenFullHD24mk430h"]} value={1100} altText="ProductIcon" />
+        <Card className="Product" title="Logitech G305" subTitle="Mouse Gamer Logite LightSpeed" imageSrc={icons["MouseGamerLogitechG305LightSpeed"]} value={350} altText="ProductIcon" />
+        <Card className="Product" title="Logitech G403 Hero" subTitle="Mouse Gamer Logitech" imageSrc={icons["MouseGamerLogitechG403Hero"]} value={378} altText="ProductIcon" />
+        <Card className="Product" title="MousePad Logitech G840" subTitle="Extra Grande" imageSrc={icons["MousePadGamerExtraGrandeLogitechG840"]} value={138} altText="ProductIcon" />
+        <Card className="Product" title="MousePad RedDragon" subTitle="Archelon P002" imageSrc={icons["MousePadGamerRedDragonArchelonP002"]} value={129} altText="ProductIcon" />
+        <Card className="Product" title="Logitech PowerPlay G943" subTitle="MousePadLogitech" imageSrc={icons["MousePadLogitechPowerPlayG943"]} value={119} altText="ProductIcon" />
+        <Card className="Product" title="Kit PCGamer Geforce" subTitle="IntelCore i5 8Gb Hd500Gb" imageSrc={icons["PCGamerIntelCore58GbHd500GbGeforceKit"]} value={2239} altText="ProductIcon" />
+        <Card className="Product" title="Logitech G213" subTitle="Logitech G213 Prodigy" imageSrc={icons["TecladoGamerLogitechG213Prodigy"]} value={219} altText="ProductIcon" />
       </section>
     </div>
   )
